e8b2324009595b95e3882943641809a8fbd1e180
Ery Hughes <ery.c.hughes@gmail.com>
2025-03-08 15:01:11 +1300

    update authors and version number

--- src/VolFe/__init__.py
+++ src/VolFe/__init__.py
@@ -1,5 +1,5 @@
-__author__ = "Ery Hughes"
-__version__ = "0.3"
+__author__ = "Ery Hughes, Pip Liggins, Penny Wieser"
+__version__ = "0.4"
 
 # functions for calculating properties of the melt and gas
 from .melt_gas import *
